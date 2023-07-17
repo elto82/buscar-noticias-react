0a2cdddb6e1e6959e55339533e43d08674b02ab8
Argiro <elto.82@gmai.com>
2023-07-16 19:29:57 -0500

    obtener datos de la api de news

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import "materialize-css/dist/css/materialize.min.css";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [categoria, setCategoria] = useState("");
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=7ab9cdb44adc454a9b4955f016630e56`;
+
+      const res = await axios(url);
+      console.log(res.data.articles);
+      setNoticias(res.data.articles);
+    };
+    consultarApi();
+  }, [categoria]);
+
   return (
     <>
       <Header titulo={"Buscador de Noticias"} />
       <div className="container whiter">
-        <Formulario />
+        <Formulario setCategoria={setCategoria} />
       </div>
     </>
   );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import styles from "./Formulario.module.css";
 import useSelect from "./hooks/useSelect";
 
-const Formulario = () => {
+const Formulario = ({ setCategoria }) => {
   const OPCIONES = [
     { value: "general", label: "General" },
     { value: "business", label: "Negocios" },
@@ -13,10 +13,18 @@ const Formulario = () => {
   ];
 
   const [categoria, SelectNoticia] = useSelect("sports", OPCIONES);
+
+  const handleBuscarNoticia = (e) => {
+    e.preventDefault();
+    if (categoria.length === 0) return;
+    setCategoria(categoria);
+    console.log(categoria);
+  };
+
   return (
     <div>
       <div className={`${styles.buscador} row`}>
-        <form className="col s12 m8 offset-m2">
+        <form onSubmit={handleBuscarNoticia} className="col s12 m8 offset-m2">
           <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
           <SelectNoticia />
 
